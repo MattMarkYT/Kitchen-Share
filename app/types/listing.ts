0302467bad8a1c419ca8c990892f8b62c998edb7
MattMark <matthew99729@gmail.com>
2026-04-13 17:05:28 -0700

    Listing now RecordModel interface

--- app/types/listing.ts
+++ app/types/listing.ts
@@ -1,14 +1,19 @@
-export type Listing = {
+import {RecordModel} from "pocketbase";
+
+export interface Listing extends RecordModel {
     id: string;
     title: string;
     price: number;
     location?: string;
     main_image?: string;
+    images?: string[];
+    description?: string;
+    tags?: string[];
     created: string;
-    seller?: string;
+    seller: string;
     expand?: {
         seller?: {
             rating?: number | null;
         };
     };
-};
+}
